96ffdc63e92a8a2a5739bc30b84843396bc29f37
jahu6304 <jahu6304@colorado.edu>
2025-03-21 15:46:37 -0400

    postgresql added

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World from jahu6304 in 3308!'
+
+@app.route('db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://jhello_user:77TALs6OnzU517oI6xqY1xWhaMCuebsA@dpg-cves002n91rc73apnhp0-a/jhello")
+    conn.close
+    return "Database Connection Successful"
