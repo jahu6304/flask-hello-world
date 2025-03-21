864fcae465faf1cf823d05a71cad29ce92ed0e07
jahu6304 <jahu6304@colorado.edu>
2025-03-21 19:43:55 -0400

    basketball table

--- app.py
+++ app.py
@@ -10,5 +10,22 @@ def hello_world():
 @app.route('/db_test')
 def testing():
     conn = psycopg2.connect("postgresql://jhello_user:77TALs6OnzU517oI6xqY1xWhaMCuebsA@dpg-cves002n91rc73apnhp0-a/jhello")
-    conn.close
+    conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def create():
+    conn = psycopg2.connect("postgresql://jhello_user:77TALs6OnzU517oI6xqY1xWhaMCuebsA@dpg-cves002n91rc73apnhp0-a/jhello")
+    cur = conn.cursor()
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS Basketball(
+                    First varchar(255),
+                    Last varchar(255),
+                    City varchar(255),
+                    Name varchar(255),
+                    Number Int                
+                    );
+                ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
